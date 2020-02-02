23f7e902a55ad82a0d012f56f4b9dca3a39e7344
Alber Erre <arblugo@gmail.com>
2020-02-02 09:34:47 +0000

    add express routes

--- app.js
+++ app.js
@@ -1,35 +1,78 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const { Email } = require("./models");
 dotenv.config();
 
+// Mongo DB
 const url = process.env.MONGO_URL;
 const mongoConnect = url => {
-  mongoose.connect(url, { useNewUrlParser: true });
+  mongoose
+    .connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    .then(() => console.log("connected to database!"))
+    .catch(err => {
+      console.log(
+        "An error ocurred while trying to connect to database: ",
+        err.message
+      );
+    });
   return mongoose.connection;
 };
 
 const client = mongoConnect(url);
-client.on("error", console.error.bind(console, "connection error:"));
-client.once("open", function() {
-  console.log("connected!");
-  //console.log("client", client);
+//client.on("error", console.error.bind(console, "connection error:"));
+// client.once("open", function() {
+//   console.log("connected!");
+// });
+
+// Express API
+const PORT = 3001;
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+app.get("/", function(req, res) {
+  res.send("welcome to fakers.ai API!");
 });
 
-// create new item
-let newEmail = new Email({
-  email: "test@fakers.ai",
-  timestamp: new Date()
+app.get("/email-listing/all", function(req, res) {
+  Email.find((err, emails) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send(err);
+    }
+    console.log("email-listing: ", emails);
+    res.status(200).send(emails);
+  });
 });
 
-//save item
-newEmail.save((err, result) => {
-  if (err) return console.error(err);
+app.post("/mailing", async (req, res) => {
+  let email = req.body.email;
+  let newEmail = new Email({
+    email,
+    timestamp: new Date()
+  });
+  await newEmail.save((err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send(err);
+    }
+  });
+  console.log("new mail added: ", email);
+  res.status(200).send("Email added to list.");
 });
 
-//return all items in collection
-
-Email.find((err, emails) => {
-  if (err) return console.error(err);
-  console.log(emails);
+app.listen(PORT, () => {
+  console.log(`API ready on port ${PORT}.`);
 });
